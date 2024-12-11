b58ef769052b24c3508ef1233bb9ca4f8d4a28ad
Sandstorm <57902127+sandstormdeveloper@users.noreply.github.com>
2024-12-11 13:39:25 +0100

    Clase 1

--- Assets/Scripts/Grupo G/QMindTester.cs
+++ Assets/Scripts/Grupo G/QMindTester.cs
@@ -1,18 +1,22 @@
+using NavigationDJIA.World;
+using QMind.Interfaces;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QMindTester : MonoBehaviour
+namespace GrupoG
 {
-    // Start is called before the first frame update
-    void Start()
+    public class QMindTester : IQMind
     {
-        
-    }
+        public void Initialize(WorldInfo worldInfo)
+        {
+            Debug.Log("QMindDummy: initialized");
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public CellInfo GetNextStep(CellInfo currentPosition, CellInfo otherPosition)
+        {
+            Debug.Log("QMindDummy: GetNextStep");
+            return null;
+        }
     }
 }
--- Assets/Scripts/Grupo G/QMindTrainer.cs
+++ Assets/Scripts/Grupo G/QMindTrainer.cs
@@ -1,18 +1,52 @@
+using NavigationDJIA.Interfaces;
+using NavigationDJIA.World;
+using QMind;
+using QMind.Interfaces;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class QMindTrainer : MonoBehaviour
+namespace GrupoG
 {
-    // Start is called before the first frame update
-    void Start()
+    public class QMindTrainer : IQMindTrainer
     {
-        
-    }
+        public int CurrentEpisode { get; }
+        public int CurrentStep { get; }
+        public CellInfo AgentPosition { get; private set; }
+        public CellInfo OtherPosition { get; private set; }
+        public float Return { get; }
+        public float ReturnAveraged { get; }
+        public event EventHandler OnEpisodeStarted;
+        public event EventHandler OnEpisodeFinished;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private QMindTrainerParams _qMindTrainerParams;
+        private WorldInfo _worldInfo;
+        private INavigationAlgorithm _navigationAlgorithm;
+
+
+        public void Initialize(QMindTrainerParams qMindTrainerParams, WorldInfo worldInfo, INavigationAlgorithm navigationAlgorithm)
+        {
+            _worldInfo = worldInfo;
+            _navigationAlgorithm = navigationAlgorithm;
+            _qMindTrainerParams = qMindTrainerParams;
+            _navigationAlgorithm.Initialize(_worldInfo);
+
+            Debug.Log("QMindTrainerDummy: initialized");
+            AgentPosition = worldInfo.RandomCell();
+            OtherPosition = worldInfo.RandomCell();
+            OnEpisodeStarted?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void DoStep(bool train)
+        {
+            int action = Random.Range(0, 4);
+            CellInfo newAgentPosition = _worldInfo.NextCell(AgentPosition, _worldInfo.AllowedMovements.FromIntValue(action));
+            CellInfo[] path = _navigationAlgorithm.GetPath(OtherPosition, AgentPosition, 1);
+            AgentPosition = newAgentPosition;
+            //OtherPosition = path[1];
+            Debug.Log("QMindTrainerDummy: DoStep");
+        }
     }
 }
