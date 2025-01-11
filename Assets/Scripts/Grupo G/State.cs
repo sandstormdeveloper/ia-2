6b94366405b77a3b6e461283ab857e9b716a4f7b
Sandstorm <57902127+sandstormdeveloper@users.noreply.github.com>
2025-01-11 21:09:22 +0100

    Update State.cs

--- Assets/Scripts/Grupo G/State.cs
+++ Assets/Scripts/Grupo G/State.cs
@@ -4,79 +4,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class State
+namespace GrupoG
 {
-    public bool NWall;
-    public bool SWall;
-    public bool EWall;
-    public bool OWall;
-
-    public bool playerAbove;
-    public bool playerRight;
-
-    public int playerDistance;
-    
-    public State(CellInfo AgentPos, CellInfo OtherPos, WorldInfo worldInfo)
+    public class State
     {
-        if (AgentPos != null && OtherPos != null && worldInfo != null)
+        public bool NWall;
+        public bool SWall;
+        public bool EWall;
+        public bool OWall;
+
+        public bool playerAbove;
+        public bool playerRight;
+
+        public int playerDistance;
+
+        public State(CellInfo AgentPos, CellInfo OtherPos, WorldInfo worldInfo)
         {
-            NWall = !worldInfo.NextCell(AgentPos, Directions.Up).Walkable;
-            SWall = !worldInfo.NextCell(AgentPos, Directions.Down).Walkable;
-            EWall = !worldInfo.NextCell(AgentPos, Directions.Right).Walkable;
-            OWall = !worldInfo.NextCell(AgentPos, Directions.Left).Walkable;
+            if (AgentPos != null && OtherPos != null && worldInfo != null)
+            {
+                NWall = !worldInfo.NextCell(AgentPos, Directions.Up).Walkable;
+                SWall = !worldInfo.NextCell(AgentPos, Directions.Down).Walkable;
+                EWall = !worldInfo.NextCell(AgentPos, Directions.Right).Walkable;
+                OWall = !worldInfo.NextCell(AgentPos, Directions.Left).Walkable;
 
-            playerAbove = OtherPos.y > AgentPos.y;
-            playerRight = OtherPos.x > AgentPos.x;
-            playerDistance = BinDistance(AgentPos.Distance(OtherPos, CellInfo.DistanceType.Euclidean));
-        } 
-    }
+                playerAbove = OtherPos.y > AgentPos.y;
+                playerRight = OtherPos.x > AgentPos.x;
+                playerDistance = BinDistance(AgentPos.Distance(OtherPos, CellInfo.DistanceType.Euclidean));
+            }
+        }
 
-    private int BinDistance(float distance)
-    {
-        if (distance < 3f) return 0;  // Cerca
-        else if (distance < 6f) return 1;  // Normal
-        return 2;  // Lejos
-    }
+        private int BinDistance(float distance)
+        {
+            if (distance < 3f) return 0;  // Cerca
+            else if (distance < 6f) return 1;  // Normal
+            return 2;  // Lejos
+        }
 
-    public override bool Equals(object obj)
-    {
-        return obj is State state &&
-               NWall == state.NWall &&
-               SWall == state.SWall &&
-               EWall == state.EWall &&
-               OWall == state.OWall &&
-               playerAbove == state.playerAbove &&
-               playerRight == state.playerRight &&
-               playerDistance == state.playerDistance;
-    }
+        public override bool Equals(object obj)
+        {
+            return obj is State state &&
+                   NWall == state.NWall &&
+                   SWall == state.SWall &&
+                   EWall == state.EWall &&
+                   OWall == state.OWall &&
+                   playerAbove == state.playerAbove &&
+                   playerRight == state.playerRight &&
+                   playerDistance == state.playerDistance;
+        }
 
-    public override int GetHashCode()
-    {
-        HashCode hash = new HashCode();
-        hash.Add(NWall);
-        hash.Add(SWall);
-        hash.Add(EWall);
-        hash.Add(OWall);
-        hash.Add(playerAbove);
-        hash.Add(playerRight);
-        hash.Add(playerDistance);
-        return hash.ToHashCode();
-    }
+        public override int GetHashCode()
+        {
+            HashCode hash = new HashCode();
+            hash.Add(NWall);
+            hash.Add(SWall);
+            hash.Add(EWall);
+            hash.Add(OWall);
+            hash.Add(playerAbove);
+            hash.Add(playerRight);
+            hash.Add(playerDistance);
+            return hash.ToHashCode();
+        }
 
-    public string StateId()
-    {
-        string id = "";
+        public string StateId()
+        {
+            string id = "";
 
-        id += NWall ? 1 : 0;
-        id += SWall ? 1 : 0;
-        id += EWall ? 1 : 0;
-        id += OWall ? 1 : 0;
+            id += NWall ? 1 : 0;
+            id += SWall ? 1 : 0;
+            id += EWall ? 1 : 0;
+            id += OWall ? 1 : 0;
 
-        id += playerAbove ? 1 : 0;
-        id += playerRight ? 1 : 0;
+            id += playerAbove ? 1 : 0;
+            id += playerRight ? 1 : 0;
 
-        id += playerDistance;
+            id += playerDistance;
 
-        return id;
+            return id;
+        }
     }
 }
